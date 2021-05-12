25555e9980fe3f4beaf6e57fd297ed21565c7ecb
ahmadfsal <ahmadfaisal0231@gmail.com>
2021-05-12 20:01:04 +0700

    update .env

--- server.js
+++ server.js
@@ -1,51 +1,57 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const path = require('path');
+require('dotenv').config({
+    path: path.resolve(__dirname, `.env.${process.env.NODE_ENV}`)
+});
 
-const app = express()
-const PORT = process.env.PORT || 8080
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 let corsOptions = {
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'https://glacial-journey-79187.herokuapp.com'
+    ],
     Headers: {
         'Access-Control-Allow-Origin': '*'
     }
-}
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-const db = require('./src/models/db')
-db.sequelize.sync()
+const db = require('./src/models/db');
+db.sequelize.sync();
 // db.sequelize.sync({ force: true }).then(() => {
 //     console.log('Drop and re-sync db.')
 // })
 
-// parse request of content-type: application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-// parse request of content-type: application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// simple route
 app.get('/', (req, res) => {
     res.json({
         message: 'Server is already running...'
-    })
-})
+    });
+});
 
-require('./src/routes/order/order')(app)
-require('./src/routes/order/order-item')(app)
-require('./src/routes/order/order-item-services')(app)
-require('./src/routes/order/order-tracker')(app)
-require('./src/routes/master/master-status')(app)
-require('./src/routes/master/master-type')(app)
-require('./src/routes/master/master-products')(app)
-require('./src/routes/master/master-services')(app)
-require('./src/routes/master/master-partnership')(app)
-require('./src/routes/master/master-promo')(app)
-require('./src/routes/customer')(app)
+require('./src/routes/order/order')(app);
+require('./src/routes/order/order-item')(app);
+require('./src/routes/order/order-item-services')(app);
+require('./src/routes/order/order-tracker')(app);
+require('./src/routes/master/master-status')(app);
+require('./src/routes/master/master-type')(app);
+require('./src/routes/master/master-products')(app);
+require('./src/routes/master/master-services')(app);
+require('./src/routes/master/master-partnership')(app);
+require('./src/routes/master/master-promo')(app);
+require('./src/routes/customer')(app);
 
 // set port, listen for request
 app.listen(PORT, () => {
-    console.log('Server is running on port: 8080')
-})
+    console.log('Server is running on port: 8080');
+});
