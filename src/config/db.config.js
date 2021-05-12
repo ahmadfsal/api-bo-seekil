8e24bce697b2caed3b70411cc4e15c2eee2b86ad
ahmadfsal <ahmadfaisal0231@gmail.com>
2021-05-12 21:06:34 +0700

    update env

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: process.env.DB_HOST,
     USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PASS,
-    DB: process.env.DB_NAME,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB,
     dialect: 'mysql',
     pool: {
         max: 5,
