127345e734a9fd7cb2c9e177de699e4d82740455
ahmadfsal <ahmadfaisal0231@gmail.com>
2021-05-12 20:51:56 +0700

    update server.js

--- server.js
+++ server.js
@@ -29,6 +29,15 @@ db.sequelize.sync();
 //     console.log('Drop and re-sync db.')
 // })
 
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+}
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
