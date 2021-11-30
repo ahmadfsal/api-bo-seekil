be498f68e6b9260d83898f083990be6f14cfe77c
ahmadfsal <ahmadfaisal0231@gmail.com>
2021-11-30 15:32:48 +0700

    update: order current month

--- src/controllers/order/order-item.js
+++ src/controllers/order/order-item.js
@@ -50,7 +50,8 @@ module.exports = {
                 { type: sequelize.QueryTypes.SELECT }
             );
             const orders = await sequelize.query(
-                `SELECT * FROM 'order' WHERE createdAt >= '${formattedFirstDay}' AND createdAt <= '${formattedLastDay}'`,
+                // `SELECT * FROM [order] WHERE createdAt >= '${formattedFirstDay}' AND createdAt <= '${formattedLastDay}'`,
+                "SELECT * FROM `order` WHERE createdAt >= " + formattedFirstDay + " AND createdAt <= " + formattedLastDay,
                 { type: sequelize.QueryTypes.SELECT }
             );
             const total_order = orders.reduce(
