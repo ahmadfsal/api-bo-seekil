dba0b4cab9a70718554da2ade12bf447d5b1643e
ahmadfsal <ahmadfaisal0231@gmail.com>
2021-11-30 08:59:37 +0700

    update: fix auth user login

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,6 +1,6 @@
 const auth = require('../../controllers/auth');
 const router = require('express').Router();
 
-router.post('/token', auth.token).post('/login', auth.customerLogin);
+router.post('/token', auth.token).post('/login', auth.login);
 
 module.exports = router;
